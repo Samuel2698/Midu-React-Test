951f7c64184ca832a61776fb0b93d068369c6db6
Samuel Velasco <samuelvelasco2698@gmail.com>
2023-01-29 10:10:38 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+// const CAT_ENDPOINT_IMAGE_URL = ` https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`;
+
+const App = () => {
+  const [fact, setFact] = useState();
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        setFact(data.fact);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>App de Gatitos</h1>
+      {fact && <p>{fact}</p>}
+    </div>
+  );
+};
+
+export default App;
