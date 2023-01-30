bd3b5814295cae16373e87ca65e0f8c776090549
Samuel Velasco <samuelvelasco2698@gmail.com>
2023-01-30 15:37:29 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import "./App.css";
 
 const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 const CAT_PREFIX_URL = "https://cataas.com/";
@@ -7,37 +8,45 @@ const App = () => {
   const [fact, setFact] = useState();
   const [imageUrl, setImageUrl] = useState();
 
+  // Para recuperar la cita al cargar la pag.
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
       .then((data) => {
         const { fact } = data;
         setFact(fact);
-
-        const threeFirtsWords = fact.split(" ", 3).join(" ");
-        console.log(threeFirtsWords);
-
-        fetch(
-          `https://cataas.com/cat/says/${threeFirtsWords}?size=50&color=red&json=true`
-        )
-          .then((res) => res.json())
-          .then((res) => {
-            const { url } = res;
-            setImageUrl(`${CAT_PREFIX_URL}${url}`);
-          });
       });
   }, []);
+
+  // Para recuperar la img cada vez que tenemos una cita nueva
+  useEffect(() => {
+    if (!fact) return;
+
+    const threeFirtsWords = fact.split(" ", 3).join(" ");
+    console.log(threeFirtsWords);
+
+    fetch(
+      `https://cataas.com/cat/says/${threeFirtsWords}?size=50&color=red&json=true`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        const { url } = res;
+        setImageUrl(`${CAT_PREFIX_URL}${url}`);
+      });
+  }, [fact]);
 
   return (
     <main>
       <h1>App de chatons uwu</h1>
-      {fact && <p>{fact}</p>}
-      {imageUrl && (
-        <img
-          src={imageUrl}
-          alt={`Image extracted using the three first words from ${fact}`}
-        />
-      )}
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt={`Image extracted using the three first words from ${fact}`}
+          />
+        )}
+      </section>
     </main>
   );
 };
